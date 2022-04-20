0704396d51f7d373dd083abfc7268688937c967d
bill <milk32158@gmail.com>
2022-04-20 15:05:45 +0800

    forcus至一半. 需學習hook概念

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+}
 export default function Todo(props) {
     const [isEditing, setEditing] = useState(false);
     const [newName, setNewName] = useState('');
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null);
 
-    function handleChange(e){
+    const wasEditing = usePrevious(isEditing);
+
+    function handleChange(e) {
         setNewName(e.target.value);
     }
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
         props.editTask(props.id, newName);
         setNewName('');
@@ -22,10 +33,16 @@ export default function Todo(props) {
                 <label className="todo-label" htmlFor={props.id}>
                     New name for {props.name}
                 </label>
-                <input id={props.id} className="todo-text" type="text" onChange={handleChange}/>
+                <input
+                    id={props.id}
+                    className="todo-text"
+                    type="text"
+                    onChange={handleChange}
+                    ref={editFieldRef}
+                />
             </div>
             <div className="btn-group">
-                <button type="button" className="btn todo-cancel" onClick={()=> setEditing(false)}>
+                <button type="button" className="btn todo-cancel" onClick={() => setEditing(false)}>
                     Cancel
                     <span className="visually-hidden">renaming {props.name}</span>
                 </button>
@@ -51,7 +68,7 @@ export default function Todo(props) {
                 </label>
             </div>
             <div className="btn-group">
-                <button type="button" className="btn" onClick={() => setEditing(true)}>
+                <button type="button" className="btn" onClick={() => setEditing(true)} ref={editButtonRef}>
                     Edit <span className="visually-hidden">{props.name}</span>
                 </button>
                 <button
@@ -64,6 +81,14 @@ export default function Todo(props) {
             </div>
         </div>
     );
+    useEffect(() => {
+        if (!wasEditing && isEditing) {
+            editFieldRef.current.focus();
+        }
+        if (wasEditing && !isEditing) {
+            editButtonRef.current.focus();
+        }
+    }, [wasEditing, isEditing]);
 
     return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 
