7a04e1451f768aec10b5912e16eb923188da283f
bill <milk32158@gmail.com>
2022-04-20 11:07:47 +0800

    TodoList完成

--- src/App.js
+++ src/App.js
@@ -4,16 +4,78 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+
+const FILTER_MAP = {
+  ALL       : () => true,
+  Active    : task  => !task.completed,  
+  Completed : task  => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('ALL')
+
+  function toggleTaskCompleted (id){
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id)
+      {
+        return {...task, completed: !task.completed} 
+      }
+      return task
+    });
+
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id){
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName){
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id)
+      {
+        return {...task, name: newName};
+      }
+
+      return task;
+
+    })
+    setTasks(editedTaskList);
+  }
+
 
   function addTask(name){
     const newTask = { id: "todo-" +　nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
-  
-  const taskList = tasks.map(task => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>
+
+  // 任務列表
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+    <Todo 
+      id={task.id} 
+      name={task.name} 
+      completed={task.completed} 
+      key={task.id} 
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  // 選單列表
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed = {name === filter}
+      setFilter = {setFilter}
+      />
   ));
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
@@ -24,9 +86,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
